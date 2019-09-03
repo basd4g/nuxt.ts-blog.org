0da3380d12486244c7fdc154cae761c9a35fe350
Keisuke Nakayama <baskk4.dt@gmail.com>
2019-09-04 00:07:56 +0900

    Delete Logo.vue

---
+++ components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="nuxt-ts-blog--header">
+      {{message}}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Header extends Vue {
+    message: String = "This is Header"
+}
+</script>
+
+<style></style>
